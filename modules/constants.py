b3891ebf1768f9a3bdf7a6e97b4aa77c585d59dd
Aleksander <awasilewski1310@gmail.com>
2022-11-11 17:08:40 +0100

    First Commit

---
+++ modules/constants.py
@@ -0,0 +1,2 @@
+THIS_SEASON_PATH = f'https://fixturedownload.com/download/epl-2022-UTC.csv'
+LAST_SEASON_PATH = f'https://fixturedownload.com/download/epl-2021-GMTStandardTime.csv'
---
+++ modules/reading.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from modules.constants import THIS_SEASON_PATH, LAST_SEASON_PATH
+import urllib.request as urlrq
+import certifi
+import ssl
+
+
+def read_url_csv(path):
+    """
+    Reads data from csv online and converts to Pandas DataFrame
+    :param path: (string) url path to data
+    :return: (Pandas DataFrame) Data
+    """
+    resp = urlrq.urlopen(path, context=ssl.create_default_context(cafile=certifi.where()))
+    df = pd.read_csv(resp)
+    return df
+
+
+def read_input_data():
+    print(f'\n Reading Data...\n')
+    df_this = read_url_csv(THIS_SEASON_PATH)
+    df_last = read_url_csv(LAST_SEASON_PATH)
+    df_data = pd.concat([df_last, df_this], ignore_index=True)
+    return df_data
+
+
+if __name__ == "__main__":
+    print(read_input_data().shape)
+
